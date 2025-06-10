194fec9821be33bb9dcec95a44cd9b7563c8b2e9
Johnn Rodríguez <johnnrodriguez0112@gmail.com>
2025-06-10 20:47:20 +0200

    feat: add MainView component to App.vue template

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Header from './components/Header/Header.vue';
+import MainView from './views/MainView.vue'
 </script>
 
 <template>
   <Header />
+  <MainView />
 </template>
 
 <style scoped>

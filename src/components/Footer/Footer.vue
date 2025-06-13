63083117f653312caa84bc90504c74d1459d26e4
Johnn Rodríguez <johnnrodriguez0112@gmail.com>
2025-06-13 10:22:27 +0200

    feat: localize footer content using translation keys

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -3,7 +3,7 @@
         <div
             class="container mx-auto px-6 flex flex-col md:flex-row justify-between items-center space-y-6 md:space-y-0">
             <div class="text-center md:text-left">
-                <h4 class="text-2xl font-semibold mb-2">crowdvigo</h4>
+                <h4 class="text-2xl font-semibold mb-2">{{ $t("footer.brand") }}</h4>
                 <p class="text-sm w-80">
                     Conectamos proyectos innovadores con inversores comprometidos. Transparencia, seguridad y compromiso
                     en cada inversión.
@@ -11,19 +11,19 @@
             </div>
 
             <div class="text-center">
-                <h5 class="font-semibold text-lg mb-2">Enlaces útiles</h5>
+                <h5 class="font-semibold text-lg mb-2">{{ $t("footer.usefulLinksTitle") }}</h5>
                 <ul class="space-y-1">
-                    <li><a href="#" class="hover:underline">Inicio</a></li>
-                    <li><a href="#" class="hover:underline">Proyectos</a></li>
-                    <li><a href="#" class="hover:underline">Tarifas</a></li>
-                    <li><a href="#" class="hover:underline">Contacto</a></li>
+                    <li><a href="#" class="hover:underline">{{ $t("footer.links.home") }}</a></li>
+                    <li><a href="#" class="hover:underline">{{ $t("footer.links.projects") }}</a></li>
+                    <li><a href="#" class="hover:underline">{{ $t("footer.links.tariffs") }}</a></li>
+                    <li><a href="#" class="hover:underline">{{ $t("footer.links.contact") }}</a></li>
                 </ul>
             </div>
 
             <div class="text-center md:text-right">
-                <h5 class="font-semibold text-lg mb-2">Contacto</h5>
-                <p>Email: contacto@crowdvigo.com</p>
-                <p>Tel: +34 123 456 789</p>
+                <h5 class="font-semibold text-lg mb-2">{{ $t("footer.contactTitle") }}</h5>
+                <p>{{ $t("footer.emailLabel") }}: {{ $t("footer.email") }}</p>
+                <p>{{ $t("footer.phoneLabel") }}: {{ $t("footer.phone") }}</p>
                 <div class="flex justify-center md:justify-end mt-4 space-x-4">
                     <img src="/icons/facebook.svg" class="w-6 h-6" alt="Facebook">
                     <img src="/icons/twitter.svg" class="w-6 h-6" alt="Twitter">
@@ -31,7 +31,7 @@
                 </div>
             </div>
         </div>
-        <p class="text-center text-sm text-gray-400 mt-6">© 2025 crowdvigo. Todos los derechos reservados.</p>
+        <p class="text-center text-sm text-gray-400 mt-6">{{ $t("footer.copyright") }}</p>
     </footer>
 </template>
 

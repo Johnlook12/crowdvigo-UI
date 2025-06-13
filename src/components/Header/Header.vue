b27e64bdcebdc714815010ff3fb1f3112a251170
Johnn Rodríguez <johnnrodriguez0112@gmail.com>
2025-06-13 10:22:43 +0200

    feat: wrap buttons in router-links for navigation in Header component

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,14 +1,20 @@
 <template>
     <div class="sticky top-0 z-50 bg-white py-5 flex flex-row border-b border-[#e5e5e5]">
         <div class="flex flex-row items-center justify-between w-full">
-            <div class="ml-5 ">
-                <img class="w-50" src="/img/logo.svg" alt="crowdvigo logo">
+            <div class="ml-5 cursor-pointer">
+                <router-link to="/">
+                    <img class="w-50" src="/img/logo.svg" alt="crowdvigo logo">
+                </router-link>
             </div>
             <div class="mr-5 space-x-8">
-                <button class="font-semibold cursor-pointer text-xl">{{ $t('login.title') }}</button>
-                <button class="p-3 pr-6 pl-6 bg-[#001136] text-[#ffffff] rounded-xl text-xl font-semibold cursor-pointer">{{
-                    $t('login.register') }}
-                </button>
+                <router-link to="/auth/login">
+                    <button class="font-semibold cursor-pointer text-xl">{{ $t('login.title') }}</button>
+                </router-link>
+                <router-link to="/auth/register">
+                    <button class="p-3 pr-6 pl-6 bg-[#001136] text-[#ffffff] rounded-xl text-xl font-semibold cursor-pointer">{{
+                        $t('login.register') }}
+                    </button>
+                </router-link>
             </div>
         </div>
         <LangComponent></LangComponent>

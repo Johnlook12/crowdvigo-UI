c4a85fb5ed750fdda516b39eb29daee9642034a4
Johnn Rodríguez <johnnrodriguez0112@gmail.com>
2025-06-13 10:22:15 +0200

    feat: update App.vue to conditionally render Header and Footer based on route meta

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
+import { useRoute } from 'vue-router';
 import Header from './components/Header/Header.vue';
-import MainView from './views/MainView.vue'
+import Footer from './components/Footer/Footer.vue';
+
+const route = useRoute();
 </script>
 
 <template>
-  <Header />
-  <MainView />
+  <Header v-if="!route.meta.hideLayout" />
+  <router-view />
+  <Footer v-if="!route.meta.hideLayout" />
 </template>
 
 <style scoped>

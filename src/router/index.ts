e2dd19c78a7de48f96d4d103d6c825d07b6aec89
Johnn Rodríguez <johnnrodriguez0112@gmail.com>
2025-06-14 17:04:34 +0200

    feat: implement route guard for authentication checks in router

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import AuthenticationView from '../views/AuthenticationView.vue'
 import LoginForm from '../components/LoginForm/LoginForm.vue'
 import RegisterForm from '../components/RegisterForm/RegisterForm.vue'
+import { useAuthStore } from '../store/AuthStore'
 
 const routes = [
     {
@@ -32,9 +33,27 @@ const routes = [
     }
 ]
 
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
+})
+
+router.beforeEach((to, from, next) => {
+  const auth = useAuthStore();
+
+  if(to.meta.requiresAuth && !auth.isAuthenticated) {
+    next({ name: 'login' });
+  }
+
+    if (to.name === 'login' && auth.isAuthenticated) {
+        next({ name: 'home' });
+    } else if (to.name === 'register' && auth.isAuthenticated) {
+        next({ name: 'home' });
+    } else {
+        next();
+    }
+
 })
 
 export default router

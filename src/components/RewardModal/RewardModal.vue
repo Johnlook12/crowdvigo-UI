c80c49045f4e7eae5be8afebaa90648e0b8848db
Johnn Rodríguez <johnnrodriguez0112@gmail.com>
2025-06-20 23:21:46 +0200

    feat: enhance RewardModal component with investment handling and error display

--- src/components/RewardModal/RewardModal.vue
+++ src/components/RewardModal/RewardModal.vue
@@ -3,7 +3,8 @@
         <!-- Overlay -->
         <div class="fixed inset-0 bg-[rgba(0,0,0,0.1)]" @click="emit('close')"></div>
         <!-- Modal box -->
-        <div class="bg-white rounded-lg shadow-lg overflow-hidden w-11/12 max-w-lg z-60 p-4 max-h-[90vh] overflow-y-auto">
+        <div
+            class="bg-white rounded-lg shadow-lg overflow-hidden w-11/12 max-w-lg z-60 p-4 max-h-[90vh] overflow-y-auto">
             <div class="flex justify-end cursor-pointer" @click="emit('close')">
                 <XMarkIcon class="w-8"></XMarkIcon>
             </div>
@@ -12,19 +13,24 @@
             </h2>
             <div class="mt-10 border bg-[white] p-6 rounded-xl border-[#e5e5e5]">
                 <h3 class="text-2xl font-semibold">Realiza una contribución</h3>
+                <span class="text-red-600 font-semibold text-lg ml-1 pt-3 block">{{ error }}</span>
                 <div class="flex flex-row items-center">
-                    <input type="number" class="w-full p-4 mt-4 border bg-[rgba(0,0,0,0.1)] border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
-                    <button class="ml-4 bg-[#001136] h-15! mt-3 px-3 text-white rounded">Continuar</button>
+                    <input v-model="investmentQuantity" type="number"
+                        class="w-full p-4 mt-4 border bg-[rgba(0,0,0,0.1)] border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
+                    <button class="ml-4 bg-[#001136] h-15! mt-3 px-3 text-white rounded"
+                        @click="handleInvestment">Continuar</button>
                 </div>
             </div>
             <section class="p-4">
                 <div>
                     <h4 class="text-xl font-semibold">Recompensas disponibles</h4>
+
                     <ul class="mt-2">
                         <li v-for="reward in rewards" :key="reward.id" class="border-b border-gray-200 py-2">
                             <h5 class="font-semibold">{{ reward.title }}</h5>
                             <p class="text-gray-600">{{ reward.description }}</p>
-                            <p class="text-gray-500">Mínimo: {{ reward.minimumAmount }} - Entrega estimada: {{ reward.estimatedDeliveryDate }}</p>
+                            <p class="text-gray-500">Mínimo: {{ reward.minimumAmount }} - Entrega estimada: {{
+                                reward.estimatedDeliveryDate }}</p>
                         </li>
                     </ul>
                 </div>
@@ -38,6 +44,11 @@ import { XMarkIcon } from '@heroicons/vue/20/solid';
 import type { Reward } from './type';
 import { useProjectStore } from '../../store/ProjectStore';
 import { onMounted, ref } from 'vue';
+import type { Wallet } from '../PayModal/type';
+import { useUserStore } from '../../store/UserStore';
+import { useAuthStore } from '../../store/AuthStore';
+import { usePaymentStore } from '../../store/PaymentStore';
+import { storeToRefs } from 'pinia';
 
 const props = withDefaults(defineProps<{
     projectId: number;
@@ -45,8 +56,20 @@ const props = withDefaults(defineProps<{
     projectId: 0
 });
 
+const investmentQuantity = ref<number>(0);
+const userStore = useUserStore();
+const authStore = useAuthStore();
+const paymentStore = usePaymentStore();
+const error = ref<string>('');
+const {wallet} = storeToRefs(userStore);
+
+const { currentUser } = storeToRefs(authStore);
+
 onMounted(() => {
     loadRewards();
+    if (currentUser.value?.id) {
+        userStore.loadWallet(currentUser.value.id);
+    }
 });
 
 const emit = defineEmits(['close']);
@@ -56,6 +79,33 @@ const rewards = ref<Reward[]>([]);
 
 const loadRewards = async () => {
     rewards.value = await projectStore.findProjectRewards(props.projectId);
+}
+
+const handleInvestment = async () => {
+    if (wallet.value === null || wallet.value.balance < investmentQuantity.value) {
+        error.value = 'Saldo insuficiente';
+        return;
+    }
+
+    const payload = {
+        projectId: props.projectId,
+        userId: currentUser.value?.id ?? 0,
+        amount: investmentQuantity.value,
+        date: new Date().toISOString()
+    };
+
+    paymentStore.addContribution(payload)
+        .then( async () => {
+            wallet.value!.balance -= investmentQuantity.value;
+            await paymentStore.withdrawFundsFromWallet(investmentQuantity.value, currentUser.value?.id ?? 0);
+            userStore.loadWallet(currentUser.value?.id ?? 0);
+            emit('close');
+        })
+        .catch((err) => {
+            console.error(err);
+            error.value = 'Error al realizar la inversión';
+        });
+
 }
 
 </script>

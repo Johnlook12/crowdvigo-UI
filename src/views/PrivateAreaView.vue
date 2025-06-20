76affc58ef0af37c21ade5313e9687430c01c61f
Johnn Rodríguez <johnnrodriguez0112@gmail.com>
2025-06-20 23:22:02 +0200

    feat: update PrivateAreaView to enhance wallet and project management features

--- src/views/PrivateAreaView.vue
+++ src/views/PrivateAreaView.vue
@@ -13,8 +13,8 @@
                 <router-link to="/private/investments">
                     <button class="py-3 text-lg font-semibold bg-[#f5f7f6] w-60 rounded-xl">Mis inversiones</button>
                 </router-link>
-                <router-link to="/private/wallet">
-                    <button class="py-3 text-lg font-semibold bg-[#f5f7f6] w-60 rounded-xl">Mi wallet</button>
+                <router-link to="/private/my-projects">
+                    <button class="py-3 text-lg font-semibold bg-[#f5f7f6] w-60 rounded-xl">Mis proyectos</button>
                 </router-link>
                 <router-link to="/private/account">
                     <button class="py-3 text-lg font-semibold bg-[#f5f7f6] w-60 rounded-xl">Mi cuenta</button>
@@ -24,10 +24,14 @@
         <div class="bg-[#fbfbfb] w-full min-h-screen px-10 flex flex-col">
             <div class="flex flex-row space-x-10 items-center justify-between border-b border-[#e5e5e5] pb-5 mt-5">
                 <div class="flex flex-row space-x-10">
-                    <button class="bg-[#e6e8ec] p-3 rounded-xl px-6 font-semibold">Recargar wallet</button>
+                    <button class="bg-[#e6e8ec] p-3 rounded-xl px-6 font-semibold" @click="showPayModal = true">Recargar
+                        wallet</button>
                     <div>
                         <p class="">Disponible para invertir</p>
-                        <span class="text-xl text-[#001136] font-bold">0,00 €</span>
+                        <span class="text-xl text-[#001136] font-bold">{{ wallet?.balance.toFixed(2) }} €</span>
+                    </div>
+                    <div class="border-l border-[#e5e5e5] pl-6">
+                        <button class="py-3 text-lg font-semibold bg-[#001136] text-white w-60 rounded-xl cursor-pointer" @click="showNewProjectModal = true">Crear proyecto</button>
                     </div>
                 </div>
                 <div>
@@ -37,11 +41,32 @@
             <router-view></router-view>
         </div>
     </div>
+    <PayModal v-if="wallet && showPayModal" :wallet="wallet" :user-id="currentUser?.id ?? 0" @close="showPayModal = false"></PayModal>
+    <NewProjectModal v-if="showNewProjectModal" @close="showNewProjectModal = false"></NewProjectModal>
 </template>
 
 <script setup lang="ts">
-
+import { ref, onMounted } from 'vue';
+import PayModal from '../components/PayModal/PayModal.vue';
 import UserMenu from '../components/UserMenu/UserMenu.vue';
+import { useAuthStore } from '../store/AuthStore';
+import { useUserStore } from '../store/UserStore';
+import { storeToRefs } from 'pinia';
+import NewProjectModal from '../components/NewProjectModal/NewProjectModal.vue';
+
+const authStore = useAuthStore();
+const userStore = useUserStore();
+const showPayModal = ref<boolean>(false);
+const showNewProjectModal = ref<boolean>(false);
+const { currentUser } = storeToRefs(authStore);
+const {wallet} = storeToRefs(userStore);
+
+onMounted(() => {
+    if(currentUser.value?.id){
+        userStore.loadWallet(currentUser.value.id);
+    }
+});
+
 
 </script>
 
